6452e37f0ced72904b2daa44d2e6c8fb99095d09
Yeliang-Radish <yeliang.shou@gmail.com>
2018-06-07 21:42:02 -0400

    Added command line arguments
    Used yargs to parse command line arguments.
    Parsed the arguments with basic functionality.

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,17 +1,26 @@
 console.log('starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+const yargs = require('yargs');
+
 const notes = require('./notes.js');
 
-// console.log(_.isString(true));
-// console.log(_.isString('Andrew'));
-var filteredArray = _.uniq(['Andrew', 1, 'Andrew', 1, 2, 3, 4]);
-console.log(filteredArray);
+const argv = yargs.argv;
+var command = argv._[0];
+// console.log(process);
+console.log('Command: ', command);
+console.log('Yargs', argv);
 
-// console.log ('result:', notes.add(9,-3));
 
-// var user = os.userInfo();
-// 
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
+if (command === 'add') {
+    notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+    notes.getAll();
+} else if (command === 'read') {
+    notes.getNote(argv.title);
+} else if (command === 'remove'){
+    notes.removeNote(argv.title);
+} else {
+    console.log('Command not recongized');
+}
